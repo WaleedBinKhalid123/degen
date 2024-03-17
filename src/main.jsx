7eadfe18d7047b2d7baf8547474015bdc779c8a4
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2024-03-18 03:44:20 +0500

    changes

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
